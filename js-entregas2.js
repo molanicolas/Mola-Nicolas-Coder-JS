57d60fe664fd2b874bf8d062fdc95cee60a63f0a
Nicolás Mola <88169405+molanicolas@users.noreply.github.com>
2022-04-07 10:49:51 -0300

    Primera Entrega
    Ciclos y Condicionales

---
+++ js-entregas2.js
@@ -0,0 +1,62 @@
+
+
+
+
+
+
+// PRIMERA ENTREGA 
+
+//   Muestra los números del 0 al 5  ITERACION FOR
+
+for (var i = 0; i < 5; i++) {
+    console.log(i);
+ }
+
+
+
+
+// determina si el nombre es o no nicolas
+
+let nombre = prompt("Ingrese un nombre")
+
+if (nombre=="Nicolas") {
+    console.log("El nombre es Nicolas");
+}
+    else{
+        console.log("el nombre no es nicolas");
+    }
+
+
+
+
+// suma 1 y muestra al llegar a 10
+
+let contador=0
+do{
+    ++contador
+}
+ while (contador==10){
+     console.log("llegaste a 10")
+ }
+
+
+
+// SEGUNDA ENTREGA 
+
+/*
+alert("CALCULADORA DE INDICE DE MASA CORPORAL")
+let estatura = parseFloat( prompt ("Ingrese su estatura en este formato EJ: 1.82 "));
+let peso = parseFloat(prompt ("Ingrese su peso en kg "));
+let resultado;
+function imc(){
+     resultado = peso / Math.pow(estatura, 2)
+        return resultado;
+}
+
+alert(imc())
+
+
+*/
+
+
+
